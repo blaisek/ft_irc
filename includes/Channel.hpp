54c90b0f43fb7139f3915d146929fd4792b519a4
Simon <simon.aeby@laendrun.ch>
2023-07-16 17:18:51 +0200

    you can't join channel that are invite only if you're not invited

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: saeby <saeby>                              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 15:49:45 by btchiman          #+#    #+#             */
-/*   Updated: 2023/07/16 16:20:35 by saeby            ###   ########.fr       */
+/*   Updated: 2023/07/16 17:05:37 by saeby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,29 +45,33 @@ class Channel
         void addBanned(int fd);
         void removeBanned(int fd);
         std::vector<int> getBanned(void) const;
-        void addInvite(int fd);
         std::vector<int> getInvite(void) const;
 		void				removeUser(std::string nick);
 		void				setMode(char mode, bool setMode);
 		std::string			getModes(void) const;
+		bool				getMode(char mode) const;
 		void				setLimit(int limit);
 		int					getLimit(void) const;
+		void				addInvited(std::string nick);
+		void				removeInvited(std::string nick);
+		std::vector<std::string>	getInvited(void) const;
+		bool				isInvited(std::string nick) const;
 
     private:
         Channel(void);
         Channel(const Channel& other);
         Channel &operator=(const Channel &other);
 
-        std::string _name;
-        std::string _topic;
-        std::string _password;
-        std::vector<Client *> _clients;
-        std::vector<Client *> _operators;
-        std::vector<int> _banned;
-        std::vector<int> _invite;
-        std::vector<std::string> _nicknames;
-		std::map<char, bool>		_modes;
-		int							_limit;
+        std::string						_name;
+        std::string						_topic;
+        std::string						_password;
+		std::vector<Client *>			_clients;
+		std::vector<Client *>			_operators;
+		std::vector<std::string>		_invited;
+        std::vector<int>				_banned;
+        std::vector<std::string>		_nicknames;
+		std::map<char, bool>			_modes;
+		int								_limit;
 };
 
 
